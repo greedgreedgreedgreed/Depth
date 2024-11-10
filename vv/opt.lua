c25ff6b0339c3f7ccdfbb3040751444267a94062
aladin <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-11-10 14:23:07 -0500

    Update opt.lua

--- vv/opt.lua
+++ vv/opt.lua
@@ -1,8 +1,1 @@
-local CorePackages = game:GetService("CorePackages")
 
-
-for _, v in ipairs(getconnections(game:GetService("LogService").MessageOut)) do
-  v:Disable()
-end
-
-CorePackages:Disable()
