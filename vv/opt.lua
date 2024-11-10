3bac8c06bdfa7d854a2df34f594cfec11a9be78e
aladin <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-11-10 14:18:46 -0500

    Update opt.lua

--- vv/opt.lua
+++ vv/opt.lua
@@ -5,10 +5,4 @@ for _, v in ipairs(getconnections(game:GetService("LogService").MessageOut)) do
   v:Disable()
 end
 
-
-if CorePackages then
-   for _, descendant in ipairs(CorePackages:GetDescendants()) do
-       descendant:Destroy()
-   end
-   CorePackages:Destroy()
-end
+CorePackages:Destroy()
